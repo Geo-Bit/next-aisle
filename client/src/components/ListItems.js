353f301501237d8642cbc935e996f5909a0926a9
George Tipton <gtip22@gmail.com>
2023-04-13 16:03:49 -0600

    ListItems bootstrap table

--- client/src/components/ListItems.js
+++ client/src/components/ListItems.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {Fragment, useEffect, useState} from "react";
 
 const ListItems = () => {
-    return <h1>Grocery Items</h1>
+    return( 
+        <Fragment>
+            {" "}
+            <table class="table mt-5 text-center">
+            <thead>
+            <tr>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            {/*<tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+            </tr>*/}
+            </tbody>
+        </table>
+        </Fragment>
+    );
 };
 
 export default ListItems;
