97b8effc75c23da226c2578d5f7d8a854c9061c7
George Tipton <gtip22@gmail.com>
2023-04-13 10:13:09 -0600

    get specific todo route

--- server/index.js
+++ server/index.js
@@ -36,7 +36,17 @@ app.get("/todos", async(req,res) => {
 })
 
 //get a todo
+app.get("/todos/:id", async(req,res) => {
+    try {
+        const {id} = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id]);
 
+        res.json(todo.rows[0]);
+
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 //update a todo
 
