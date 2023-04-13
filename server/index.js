b1bbf38ebb35e138a773b18551c78381e764024a
George Tipton <gtip22@gmail.com>
2023-04-13 10:09:07 -0600

    get todos route

--- server/index.js
+++ server/index.js
@@ -14,19 +14,29 @@ app.post("/todos", async(req,res) =>{
     try {
         const { description } = req.body;
         const newTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1)", 
+            "INSERT INTO todo (description) VALUES($1) RETURNING * ",
             [description]
         );
 
-        res.json(newTodo)
+        res.json(newTodo.rows[0]);
+
     } catch (error) {
         console.error(error.message);
     }
 })
 
 //get all todos
+app.get("/todos", async(req,res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 //get a todo
+
 
 //update a todo
 
