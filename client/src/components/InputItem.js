72d067350d29b843a13120433c1f683f6c453687
George Tipton <gtip22@gmail.com>
2023-04-13 11:01:20 -0600

    initial InputItem component

--- client/src/components/InputItem.js
+++ client/src/components/InputItem.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 const InputItem = () => {
     return(
-        <h1>Input Item</h1>
-    )
-}
+        <Fragment>
+            <h1 className="text-center mt-5">Shopping List</h1>
+            <form className="d-flex mt-5">
+                <input type="text" className="form-control" />
+                <button className="btn btn-success">Add</button>
+            </form>
+        </Fragment>
+    );
+};
 
 export default InputItem;
