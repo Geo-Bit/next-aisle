6fc380e0ab876201444189b900b5e2f73a904fa0
George Tipton <gtip22@gmail.com>
2023-04-13 10:52:21 -0600

    client init

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import './App.css';
+
+//components
+
+import InputItem from "./components/InputItem";
+
+function App() {
+  return (
+    <Fragment>
+      <InputItem />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const InputItem = () => {
+    return(
+        <h1>Input Item</h1>
+    )
+}
+
+export default InputItem;
