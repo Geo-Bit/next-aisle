5ed3320297ecac4567f8882b80135f7180fcda59
Geo <gtip22@gmail.com>
2024-07-14 14:58:06 -0600

    updated CORS

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:3500", // Ensure this matches your frontend's URL
+    origin: ["http://localhost:3500", "http://10.0.1.162:3500"], // Ensure this matches your frontend's URL
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type"],
   })
