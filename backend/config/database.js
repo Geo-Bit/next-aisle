03ab8b99c402c1dbf68753f2416abbfb73ad47d9
Geo <gtip22@gmail.com>
2024-07-16 12:59:01 -0600

    adding debugging for backend sequelize

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
+const databaseUrl = process.env.DATABASE_URL;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
 });
+
+console.log("DATABASE_URL:", databaseUrl);
 
 module.exports = sequelize;
